d2864a8fdb7fcc71cd61e3d26b29910a1a3b60de
hiroya-sawakame <kame.2146@gmail.com>
2020-03-07 10:39:08 +0900

    README

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root to: 'home#index'
+    # APIコントローラへのルーティング
+  namespace :api, {format: 'json'} do
+    namespace :v1 do
+      resources :employees, only: [:index, :show, :create, :update, :destroy]
+    end
+  end
+end
